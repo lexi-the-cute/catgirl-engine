110ebb354fe2134d5977565311b9ca53b9849548
Alexis <alexis@catgirl.land>
2023-04-18 12:47:20 -0400

    Saving Current Work

--- src/game/render.rs
+++ src/game/render.rs
@@ -1,19 +1,23 @@
-// https://sunjay.dev/learn-game-dev/rendering-an-image.html
+// https://sunjay.dev/learn-game-dev/refactor-player-struct.html
 
 use std::thread;
 use std::time::Duration;
 
-use sdl2::render::Canvas;
-use sdl2::video::Window;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Canvas, TextureCreator, Texture};
+use sdl2::video::{Window, WindowContext};
 use sdl2::{VideoSubsystem, Sdl, EventPump};
 use sdl2::event::Event;
 use sdl2::pixels::Color;
+use sdl2::image::{self, LoadTexture, InitFlag, Sdl2ImageContext};
 use sdl2::keyboard::Keycode;
 
 // This thread handles both rendering and input (aka the client)
 pub fn start() -> Result<(), String> {
     let sdl_context: Sdl = sdl2::init()?;
     let video_subsystem: VideoSubsystem = sdl_context.video()?;
+
+    let _image_context: Sdl2ImageContext = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
     let window: Window = video_subsystem.window("Alexis' Game Engine", 800, 600)
         .position_centered()
@@ -26,6 +30,12 @@ pub fn start() -> Result<(), String> {
     canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.clear();
     canvas.present();
+
+    let texture_creator: TextureCreator<WindowContext> = canvas.texture_creator();
+    let texture: Texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let position = Point::new(0, 0);
+    let sprite = Rect::new(0, 0, 26, 36);
 
     let mut event_pump: EventPump = sdl_context.event_pump()?;
     let mut i: u8 = 0;
@@ -43,7 +53,7 @@ pub fn start() -> Result<(), String> {
         
         // Update Screen
         i = (i + 1) % 255;
-        render(&mut canvas, Color::RGB(i, 64, 255 - i));
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
 
         // Slow Down Rendering (60 FPS)
         thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
@@ -53,8 +63,16 @@ pub fn start() -> Result<(), String> {
 }
 
 // Unused, May Move Render/Event Loop Here
-fn render(canvas: &mut Canvas<Window>, color: Color) {
+fn render(canvas: &mut Canvas<Window>, color: Color, texture: &Texture, position: Point, sprite: Rect) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+
+    let (width, height) = canvas.output_size()?;
+    let screen_position: Point = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect: Rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+
+    canvas.copy(texture, sprite, screen_rect)?;
     canvas.present();
+
+    Ok(())
 }
