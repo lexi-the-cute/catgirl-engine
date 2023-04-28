02095e151b377b757f97624c773b9aae66e2b9fb
Alexis <alexis@catgirl.land>
2023-04-27 21:11:31 -0400

    Trying Musl Again

---
+++ .toolchains/musl-x86_64.cmake
@@ -0,0 +1,17 @@
+# the name of the target operating system
+set(CMAKE_SYSTEM_NAME musl-x86_64)
+
+# which compilers to use for C and C++
+set(CMAKE_C_COMPILER musl-gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+# where is the target environment located
+set(CMAKE_FIND_ROOT_PATH /usr/bin)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
