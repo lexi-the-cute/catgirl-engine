6794682cdc882ded379473f1655d00af06a2e61e
Alexis <120563837+alexisart@users.noreply.github.com>
2023-04-30 02:07:05 -0400

    Trying New Path

--- .toolchains/TC-musl-gcc.cmake
+++ .toolchains/TC-musl-gcc.cmake
@@ -6,7 +6,7 @@ set(CMAKE_C_COMPILER   x86_64-linux-musl-gcc)
 set(CMAKE_CXX_COMPILER x86_64-linux-musl-g++)
 
 # where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  ~/x86_64-linux-musl-cross/bin/)
+set(CMAKE_FIND_ROOT_PATH $ENV{HOME}/x86_64-linux-musl-cross/bin)
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
