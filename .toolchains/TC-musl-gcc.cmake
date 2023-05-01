5a181ba54f66fad248b415cff462da653af238c0
Alexis <120563837+alexisart@users.noreply.github.com>
2023-04-30 23:33:33 -0400

    Testing libc6-dev-amd64-cross

--- .toolchains/TC-musl-gcc.cmake
+++ .toolchains/TC-musl-gcc.cmake
@@ -6,7 +6,8 @@ set(CMAKE_C_COMPILER   x86_64-linux-musl-gcc)
 set(CMAKE_CXX_COMPILER x86_64-linux-musl-g++)
 
 # where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  $ENV{WORKSPACE}/build/x86-64-linux-musl-cross/x86_64-linux-musl/include;$ENV{WORKSPACE}/build/x86-64-linux-musl-cross)
+# TODO: Move From /usr/x86_64-linux-gnu/include to /usr/x86_64-linux-musl/include
+set(CMAKE_FIND_ROOT_PATH  $ENV{WORKSPACE}/build/x86-64-linux-musl-cross/x86_64-linux-musl/include;/usr/x86_64-linux-gnu/include;$ENV{WORKSPACE}/build/x86-64-linux-musl-cross)
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
