11544d218094ea4c3a825beb42b5df2a07653240
Alexis <alexis@catgirl.land>
2023-04-30 22:14:11 -0400

    Trying Build As Zip

--- .toolchains/TC-musl-gcc.cmake
+++ .toolchains/TC-musl-gcc.cmake
@@ -6,16 +6,16 @@ set(CMAKE_C_COMPILER   x86_64-linux-musl-gcc)
 set(CMAKE_CXX_COMPILER x86_64-linux-musl-g++)
 
 # where is the target environment located
-set(CMAKE_FIND_ROOT_PATH  /)
+set(CMAKE_FIND_ROOT_PATH  $ENV{WORKSPACE}/build/x86-64-linux-musl-cross/x86_64-linux-musl/include;$ENV{WORKSPACE}/build/x86-64-linux-musl-cross)
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NONE)
 
 # search headers and libraries in the target environment
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE BOTH)
 
 # Cross Compiler Stuff
-SET (CMAKE_C_COMPILER_WORKS 1)
-SET (CMAKE_CXX_COMPILER_WORKS 1)
+# SET (CMAKE_C_COMPILER_WORKS 1)
+# SET (CMAKE_CXX_COMPILER_WORKS 1)
