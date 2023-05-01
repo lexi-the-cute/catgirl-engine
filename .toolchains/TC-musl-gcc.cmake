90671804cc9c872519a70f742f5bbab9894e81f3
Alexis <120563837+alexisart@users.noreply.github.com>
2023-04-30 23:44:46 -0400

    Hmmmm

--- .toolchains/TC-musl-gcc.cmake
+++ .toolchains/TC-musl-gcc.cmake
@@ -7,7 +7,7 @@ set(CMAKE_CXX_COMPILER x86_64-linux-musl-g++)
 
 # where is the target environment located
 # TODO: Move From /usr/x86_64-linux-gnu/include to /usr/x86_64-linux-musl/include
-set(CMAKE_FIND_ROOT_PATH  $ENV{WORKSPACE}/build/x86-64-linux-musl-cross/x86_64-linux-musl/include /usr/x86_64-linux-gnu/include $ENV{WORKSPACE}/build/x86-64-linux-musl-cross)
+set(CMAKE_FIND_ROOT_PATH  "/usr/x86_64-linux-gnu/include;$ENV{WORKSPACE}/build/x86-64-linux-musl-cross/x86_64-linux-musl/include;$ENV{WORKSPACE}/build/x86-64-linux-musl-cross")
 
 # adjust the default behavior of the FIND_XXX() commands:
 # search programs in the host environment
