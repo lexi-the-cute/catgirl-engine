928032e3b24b378d0056e422ea23a5ecfce9dcb8
Alexis <alexis@catgirl.land>
2023-05-01 04:36:08 -0400

    Simplifying Workaround

--- .toolchains/TC-musl-gcc.cmake
+++ .toolchains/TC-musl-gcc.cmake
@@ -6,7 +6,7 @@ set(CMAKE_C_COMPILER   x86_64-linux-musl-gcc)
 set(CMAKE_CXX_COMPILER x86_64-linux-musl-g++)
 
 # Set Root Paths
-set(CMAKE_FIND_ROOT_PATH $ENV{WORKSPACE}/build/x86-64-linux-musl-cross $ENV{WORKSPACE}/build/x86-64-linux-musl-cross/x86_64-linux-musl /usr/x86_64-linux-gnu)
+set(CMAKE_FIND_ROOT_PATH $ENV{WORKSPACE}/build/x86-64-linux-musl-cross $ENV{WORKSPACE}/build/x86-64-linux-musl-cross/x86_64-linux-musl)  #  /usr/x86_64-linux-gnu
 
 # Set System Paths
 # set(CMAKE_SYSTEM_INCLUDE_PATH /usr/x86_64-linux-gnu/include)  # /include
@@ -16,6 +16,6 @@ set(CMAKE_FIND_ROOT_PATH $ENV{WORKSPACE}/build/x86-64-linux-musl-cross $ENV{WORK
 # NEVER Means Don't Use CMAKE_FIND_ROOT_PATH,
 # Only Means Only Use CMAKE_FIND_ROOT_PATH
 # Both Means Use Both Standard Paths and CMAKE_FIND_ROOT_PATH
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY BOTH)  # /lib
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)  # /lib
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)  # /include
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)  # /bin
