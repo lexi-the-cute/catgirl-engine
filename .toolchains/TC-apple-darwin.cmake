9053d1974b66af9981d7a280dfb97b640db9617a
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-12 01:23:52 -0400

    /tmp/build-scripts

--- .toolchains/TC-apple-darwin.cmake
+++ .toolchains/TC-apple-darwin.cmake
@@ -3,11 +3,11 @@ set(CMAKE_SYSTEM_NAME Linux)
 
 # C and C++ Compilers as well as Static Linker (AR)
 set(CMAKE_AR   ar)  # /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ar
-set(CMAKE_C_COMPILER   clang-fat.sh)
-set(CMAKE_CXX_COMPILER clang++-fat.sh)
+set(CMAKE_C_COMPILER   /tmp/build-scripts/clang-fat.sh)
+set(CMAKE_CXX_COMPILER /tmp/build-scripts/clang++-fat.sh)
 
 # Set Root Paths
-set(CMAKE_FIND_ROOT_PATH /tmp/build-scripts /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin /usr/local/include)
+set(CMAKE_FIND_ROOT_PATH /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin /usr/local/include)
 
 # NEVER Means Don't Use CMAKE_FIND_ROOT_PATH,
 # Only Means Only Use CMAKE_FIND_ROOT_PATH
