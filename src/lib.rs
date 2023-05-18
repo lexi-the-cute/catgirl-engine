da67174bbe18b53ba1e28061d52da9a7a4b0c952
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-18 02:34:03 -0400

    Saving Current Work

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[macro_use] extern crate log;
 
 use sdl2::libc;
+use wasm_bindgen::prelude::*;
 
 mod game;
 mod android;
@@ -10,4 +11,12 @@ mod android;
 pub extern fn SDL_main(_argc: libc::c_int, _argv: *const *const libc::c_char) -> libc::c_int {
     game::start();
     return 0;
+}
+
+// Run as Library (e.g. Webassembly)
+#[wasm_bindgen(start)]
+fn wasm_init() -> Result<(), JsValue> {
+    game::start();
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@
 mod game;
 
 // Run as Executable (e.g. Linux)
-fn main() {
+pub fn main() {
     game::start();
 }
