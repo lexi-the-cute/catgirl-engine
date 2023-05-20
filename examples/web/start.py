587073f34ac3c5e0b8c2624520129cc6de54ac26
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-20 02:31:00 -0400

    Fixing Some SDL Compilation Issues

---
+++ examples/web/start.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+
+hostName = "localhost"
+serverPort = 8080
+
+script_dir: str = os.path.realpath(os.path.dirname(__file__))
+files: dict = {
+    "/": os.path.join(script_dir, "index.html"),
+    "/catgirl-engine.js": os.path.join(script_dir, "catgirl-engine.js")
+}
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":        
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
