37eb9d8bbca0ecc92836bfd1724de73d782439e5
Alexis <alexis@catgirl.land>
2023-05-19 22:39:33 -0400

    Added Helper Cleaning Script

---
+++ helper/clean.sh
@@ -0,0 +1,13 @@
+cd `dirname "$0"`
+cd ..
+export WORKSPACE=`pwd`
+cargo clean
+
+cd android
+./gradlew clean
+
+rm -rf $WORKSPACE/android/app/src/main/jniLibs
+rm -rf $WORKSPACE/android/app/jni/build
+rm -rf $WORKSPACE/android/app/jni/*/build
+rm -rf $WORKSPACE/build
+rm -rf $WORKSPACE/target
