28cc869e64c7dc6aa5c699184d83ef2f3078535c
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-22 03:42:09 -0400

    Fixing Clean Script

--- helper/clean.sh
+++ helper/clean.sh
@@ -1,14 +1,14 @@
 cd `dirname "$0"`
 cd ..
 export WORKSPACE=`pwd`
-cargo clean
-
-cd android
-./gradlew clean
-
 rm -rf $WORKSPACE/android/app/src/main/jniLibs
 rm -rf $WORKSPACE/android/app/jni/build
 rm -rf $WORKSPACE/android/app/jni/*/build
 rm -rf $WORKSPACE/android/app/jni/*/external/*/build
 rm -rf $WORKSPACE/build
 rm -rf $WORKSPACE/target
+
+cargo clean
+
+cd android
+./gradlew clean
