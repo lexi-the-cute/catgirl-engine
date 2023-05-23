2947305fc5506bb448b8a2f91066c335cfd65a68
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-23 01:12:02 -0400

    Updating Demo Server

--- examples/web/start.py
+++ examples/web/start.py
@@ -8,24 +8,58 @@ serverPort = 8080
 
 script_dir: str = os.path.realpath(os.path.dirname(__file__))
 files: dict = {
-    "/": os.path.join(script_dir, "index.html"),
-    "/catgirl-engine.js": os.path.join(script_dir, "catgirl-engine.js")
+    "/": {
+        "path": os.path.join(script_dir, "index.html"),
+        "content-type": "text/html"
+    },
+    "/main.js": {
+        "path": os.path.join(script_dir, "main.js"),
+        "content-type": "text/javascript"
+    },
+    "/main.wasm": {
+        "path": os.path.join(script_dir, "main.wasm"),
+        "content-type": "application/wasm"
+    }
 }
 
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
+    def send_404(self):
+        self.send_response(404)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+
+        self.wfile.write(bytes("<html><head><title>404 - File Not Found</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("<h1>404 - File Not Found</h1>", "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
+
+    def send_file(self, file_path: str, content_type: str):
+        self.send_response(200)
+        self.send_header("Content-type", content_type)
+        self.end_headers()
+
+        with open(file=file_path, mode="rb") as f:
+            self.wfile.write(f.read())
+
+    def get_file(self, url_path: str):
+        if url_path in files:
+            if os.path.exists(files[url_path]["path"]):
+                self.send_file(file_path = files[url_path]["path"], content_type=files[url_path]["content-type"])
+            else:
+                self.send_404()
+        else:
+            self.send_404()
+
+    def do_GET(self):
+        self.get_file(url_path=self.path)
 
 if __name__ == "__main__":        
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
+    print("-"*40)
+    print("This is not a production server. It only exists for demonstration purposes. Use a production server instead")
+    print("-"*40)
 
     try:
         webServer.serve_forever()
