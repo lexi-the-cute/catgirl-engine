e0013ff18127b69dda0a0824bfa775fdcb9c15f4
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-23 00:19:23 -0400

    Handling Emscripten in build.rs

--- build.rs
+++ build.rs
@@ -1,17 +1,34 @@
 extern crate cbindgen;
 
-use std::env;
+use std::env::{self, Vars};
 use std::path::PathBuf;
 use cbindgen::{Config, Language};
 
 fn main() {
-    create_js();
+    // For some reason, the cfg!() macros won't cooperate, so Alexis is doing this herself
+    let target_arch: String = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_os: String = env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    // Only Emscripten builds need the javascript generation flag set
+    if (target_arch == "wasm32" || target_arch == "wasm64") && target_os == "emscripten" {
+        create_emscripten_wasm();
+    }
+
+    // Bindings are only usable when building libs
     create_bindings();
 }
 
-fn create_js() {
+fn create_emscripten_wasm() {
     // This is only to run for the wasm32-unknown-emscripten target
-    println!("cargo:rustc-link-arg=--oformat=js");
+    // println!("cargo:warning=Building Emscripten Wasm");
+    let output_file: String = target_dir()
+    .join("wasm")
+    .join(format!("{}.{}", "main", "js"))
+    .display()
+    .to_string();
+
+    println!("cargo:rustc-env=EMCC_CFLAGS=-s ERROR_ON_UNDEFINED_SYMBOLS=0 --no-entry");
+    println!("cargo:rustc-link-arg=-o{output_file}");
 }
 
 fn create_bindings() {
@@ -49,5 +66,14 @@ fn target_dir() -> PathBuf {
         PathBuf::from(target)
     } else {
         PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("target")
+    }
+}
+
+#[allow(dead_code)]
+fn print_environment_vars() {
+    let vars: Vars = std::env::vars();
+
+    for (key, var) in vars {
+        println!("cargo:warning=EV: {key}: {var}");
     }
 }
