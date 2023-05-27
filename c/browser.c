559b821c57bc11f6ed87924dd11680512e3bd3d1
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-27 08:31:15 -0400

    Attempting To Communicate With Web Worker

--- c/browser.c
+++ c/browser.c
@@ -3,30 +3,50 @@
 
 void trace(char* message) {
     EM_ASM({
-        console.trace(UTF8ToString($0, $1));
+        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
+            console.trace(UTF8ToString($0, $1));
+        } else if (ENVIRONMENT_IS_WORKER) {
+            postMessage(UTF8ToString($0, $1));
+        }
     }, message, strlen(message));
 }
 
 void debug(char* message) {
     EM_ASM({
-        console.debug(UTF8ToString($0, $1));
+        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
+            console.debug(UTF8ToString($0, $1));
+        } else if (ENVIRONMENT_IS_WORKER) {
+            postMessage(UTF8ToString($0, $1));
+        }
     }, message, strlen(message));
 }
 
 void info(char* message) {
     EM_ASM({
-        console.info(UTF8ToString($0, $1));
+        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
+            console.info(UTF8ToString($0, $1));
+        } else if (ENVIRONMENT_IS_WORKER) {
+            postMessage(UTF8ToString($0, $1));
+        }
     }, message, strlen(message));
 }
 
 void warn(char* message) {
     EM_ASM({
-        console.warn(UTF8ToString($0, $1));
+        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
+            console.warn(UTF8ToString($0, $1));
+        } else if (ENVIRONMENT_IS_WORKER) {
+            postMessage(UTF8ToString($0, $1));
+        }
     }, message, strlen(message));
 }
 
 void error(char* message) {
     EM_ASM({
-        console.error(UTF8ToString($0, $1));
+        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
+            console.error(UTF8ToString($0, $1));
+        } else if (ENVIRONMENT_IS_WORKER) {
+            postMessage(UTF8ToString($0, $1));
+        }
     }, message, strlen(message));
 }
