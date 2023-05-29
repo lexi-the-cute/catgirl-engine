7d7ec7424497e1fd10fd498e711de1d209ae1826
Alexis <alexis@catgirl.land>
2023-05-29 04:26:22 -0400

    Fixed Logging

--- c/browser.c
+++ c/browser.c
@@ -3,75 +3,30 @@
 
 void trace(char* message) {
     EM_ASM({
-        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
-            console.trace(UTF8ToString($0, $1));
-        } else if (ENVIRONMENT_IS_WORKER) {
-            let data = {};
-            data['cmd'] = "callHandler";
-            data['handler'] = "trace";
-            data['args'] = UTF8ToString($0, $1);
-
-            postMessage(data);
-        }
+        console.trace(UTF8ToString($0, $1));
     }, message, strlen(message));
 }
 
 void debug(char* message) {
     EM_ASM({
-        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
-            console.debug(UTF8ToString($0, $1));
-        } else if (ENVIRONMENT_IS_WORKER) {
-            let data = {};
-            data['cmd'] = "callHandler";
-            data['handler'] = "debug";
-            data['args'] = UTF8ToString($0, $1);
-
-            this.postMessage(data);
-        }
+        console.debug(UTF8ToString($0, $1));
     }, message, strlen(message));
 }
 
 void info(char* message) {
     EM_ASM({
-        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
-            console.info(UTF8ToString($0, $1));
-        } else if (ENVIRONMENT_IS_WORKER) {
-            let data = {};
-            data['cmd'] = "callHandler";
-            data['handler'] = "info";
-            data['args'] = UTF8ToString($0, $1);
-
-            postMessage(data);
-        }
+        console.info(UTF8ToString($0, $1));
     }, message, strlen(message));
 }
 
 void warn(char* message) {
     EM_ASM({
-        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
-            console.warn(UTF8ToString($0, $1));
-        } else if (ENVIRONMENT_IS_WORKER) {
-            let data = {};
-            data['cmd'] = "callHandler";
-            data['handler'] = "warn";
-            data['args'] = UTF8ToString($0, $1);
-
-            postMessage(data);
-        }
+        console.warn(UTF8ToString($0, $1));
     }, message, strlen(message));
 }
 
 void error(char* message) {
     EM_ASM({
-        if (ENVIRONMENT_IS_WEB || ENVIRONMENT_IS_NODE) {
-            console.error(UTF8ToString($0, $1));
-        } else if (ENVIRONMENT_IS_WORKER) {
-            let data = {};
-            data['cmd'] = "callHandler";
-            data['handler'] = "error";
-            data['args'] = UTF8ToString($0, $1);
-
-            postMessage(data);
-        }
+        console.error(UTF8ToString($0, $1));
     }, message, strlen(message));
 }
