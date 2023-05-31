08028bd845f0ee7f34c1c4b9813125a81905e4d7
Alexis <120563837+alexisart@users.noreply.github.com>
2023-05-31 10:33:23 -0400

    Saving Current Work

--- src/game/render.rs
+++ src/game/render.rs
@@ -1,8 +1,8 @@
 // https://sunjay.dev/learn-game-dev/refactor-player-struct.html
 
+use std::sync::atomic::{AtomicU8, Ordering};
 use std::sync::mpsc::{Sender, Receiver};
 use std::thread;
-use std::sync::{Mutex, OnceLock};
 use std::time::Duration;
 
 use sdl2::controller::{Button};
@@ -18,16 +18,36 @@ use sdl2::keyboard::Keycode;
 
 use crate::game::entity::player::{Player, Direction};
 
-// static LOOPSTRUCT: OnceLock<RenderLoopStruct> = OnceLock::new();
-
 extern "C" {
     // emscripten_set_main_loop_arg(em_arg_callback_func func, void *arg, int fps, int simulate_infinite_loop)
     #[cfg(all(target_family="wasm", target_os="emscripten"))]
-    pub fn emscripten_set_main_loop(
-        func: extern "C" fn() -> bool,
+    pub fn emscripten_set_main_loop_arg(
+        func: extern "C" fn(&mut RenderLoopStruct) -> bool,
+        arg: RenderLoopStruct,
         fps: std::os::raw::c_int,
         simulate_infinite_loop: std::os::raw::c_int
     );
+}
+
+#[repr(C)]
+pub struct RenderLoopStruct<'a> {
+    pub receive: Receiver<()>,  // Receive From Main Thread In Render Thread
+    pub i: AtomicU8,  // Used for changing background color,
+
+    // SDL Related Vars
+    pub sdl_context: Sdl,
+    pub game_controller_subsystem: GameControllerSubsystem,
+    pub haptic_subsystem: HapticSubsystem,
+    pub video_subsystem: VideoSubsystem,
+    pub image_context: Sdl2ImageContext,
+    pub canvas: Canvas<Window>,
+    pub texture_creator: &'a TextureCreator<WindowContext>,
+    pub texture: Texture<'a>,
+    pub event_pump: EventPump,
+
+    // TODO: Move to Server (Physics) Thread
+    pub player: Player,
+    pub player_movement_speed: i32
 }
 
 // This thread handles both rendering and input (aka the client)
@@ -42,29 +62,27 @@ pub fn start(tx: Sender<()>, rx: Receiver<()>) {
 fn run(rx: Receiver<()>) -> Result<(), String> {
     debug!("run(rx) on render called...");
 
-    // TODO: Make Sure These Lines Are Only Initialized Once
+    // TODO: Make Sure These Are Only Initialized Once
     let sdl_context: Sdl = sdl2::init()?;
     debug!("SDL2 Initialized...");
 
     let game_controller_subsystem: GameControllerSubsystem = sdl_context.game_controller()?;
     debug!("Game Controller Subsystem Initialized...");
 
-    let _haptic_subsystem: HapticSubsystem = sdl_context.haptic()?;
+    let haptic_subsystem: HapticSubsystem = sdl_context.haptic()?;
     debug!("Haptic Subsystem Initialized...");
 
     let video_subsystem: VideoSubsystem = sdl_context.video()?;
     debug!("Video Subsystem Initialized...");
 
-    let _image_context: Sdl2ImageContext = image::init(InitFlag::PNG | InitFlag::JPG)?;
+    let image_context: Sdl2ImageContext = image::init(InitFlag::PNG | InitFlag::JPG)?;
     debug!("Image Context Initialized...");
 
     let window: Window = video_subsystem.window("Alexis' Game Engine", 800, 600)
-        .position_centered()
-        .build().expect("Could Not Make A Window");
+                                        .position_centered().build().expect("Could Not Make A Window");
     debug!("Window Created...");
 
-    let mut canvas: Canvas<Window> = window.into_canvas()
-        .build().expect("Could Not Make a Canvas");
+    let mut canvas: Canvas<Window> = window.into_canvas().build().expect("Could Not Make a Canvas");
     debug!("Canvas Created...");
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
@@ -77,12 +95,14 @@ fn run(rx: Receiver<()>) -> Result<(), String> {
 
     // `assets/bardo.png` automatically translates to `/data/data/land.catgirl.engine/files/assets/bardo.png` on Android
     // Android returns an empty string for this particular asset error while the error works as intended on Linux
-    let texture: Texture = texture_creator.load_texture("assets/bardo.png")?;
+    let texture: Texture = texture_creator.load_texture("assets/bardo.png").unwrap();
     debug!("Texture Loaded...");
 
-    // TODO: Move To Server Thread
-    const PLAYER_MOVEMENT_SPEED: i32 = 20;
-    let mut player: Player = Player {
+    let event_pump: EventPump = sdl_context.event_pump()?;
+    debug!("Event Pump Retrieved...");
+
+    // TODO: Move To Server (Physics) Thread
+    let player: Player = Player {
         position: Point::new(0, 0),
         sprite: Rect::new(0, 0, 26, 36),
         speed: 0,
@@ -90,87 +110,125 @@ fn run(rx: Receiver<()>) -> Result<(), String> {
         // inventory: Inventory {}
     };
 
-    let mut event_pump: EventPump = sdl_context.event_pump()?;
-    debug!("Event Pump Retrieved...");
-
-    let mut i: u8 = 0;
+    #[allow(unused_mut)]
+    let mut loopstruct: RenderLoopStruct = RenderLoopStruct {
+        receive: rx.into(), i: AtomicU8::new(0),
+        sdl_context,
+        game_controller_subsystem, haptic_subsystem,
+        video_subsystem, image_context,
+        canvas,
+        texture_creator: &texture_creator, texture,
+        event_pump,
+        
+        // TODO: Move To Server (Physics) Thread
+        player,
+        player_movement_speed: 20
+    };
 
     debug!("Starting Render Loop...");
-    'running: loop {
-        match rx.try_recv() {
-            Ok(_) => {
-                debug!("Terminating Render Thread...");
-                break 'running;
-            }
-            Err(_) => {
-                // Not Implemented At The Moment
-            }
+    #[cfg(all(target_family="wasm", target_os="emscripten"))]
+    unsafe {
+        emscripten_set_main_loop_arg(render_loop, loopstruct, -1, 0);
+    }
+    
+    #[cfg(not(all(target_family="wasm", target_os="emscripten")))]
+    loop {
+        let exit_loop: bool = render_loop(&mut loopstruct);
+        if exit_loop {
+            // Ending Loop
+            break;
         }
-
-        // Handle Events
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    debug!("Terminating Render Thread (Quit)...");
-                    break 'running;
-                },
-                Event::ControllerDeviceAdded { which: controller_id, .. } => {
-                    let controller_name: String = game_controller_subsystem.name_for_index(controller_id).unwrap();
-                    debug!("Controller {} ({}) Added...", controller_id, controller_name);
-                    
-                    // TODO: Determine Why Controller Input/Output Error
-                    game_controller_subsystem.is_game_controller(controller_id);
-                    game_controller_subsystem.open(controller_id).unwrap();
-                    // .set_rumble(1000, 1000, 1000).unwrap();
-                },
-                Event::ControllerDeviceRemoved { which: controller_id, .. } => {
-                    debug!("Controller {} Removed...", controller_id);
-                },
-                Event::ControllerButtonDown { button: Button::DPadLeft, .. } |
-                Event::KeyDown { keycode: Some(Keycode::Left), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Left;
-                },
-                Event::ControllerButtonDown { button: Button::DPadRight, .. } |
-                Event::KeyDown { keycode: Some(Keycode::Right), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Right;
-                },
-                Event::ControllerButtonDown { button: Button::DPadUp, .. } |
-                Event::KeyDown { keycode: Some(Keycode::Up), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Up;
-                },
-                Event::ControllerButtonDown { button: Button::DPadDown, .. } |
-                Event::KeyDown { keycode: Some(Keycode::Down), repeat: false, .. } => {
-                    player.speed = PLAYER_MOVEMENT_SPEED;
-                    player.direction = Direction::Down;
-                },
-                Event::KeyUp { keycode: Some(Keycode::Left), repeat: false, .. } |
-                Event::KeyUp { keycode: Some(Keycode::Right), repeat: false, .. } |
-                Event::KeyUp { keycode: Some(Keycode::Up), repeat: false, .. } |
-                Event::KeyUp { keycode: Some(Keycode::Down), repeat: false, .. } => {
-                    player.speed = 0;
-                    player.direction = Direction::None;
-                },
-                _ => {}
-            }
-        }
-        
-        // Update Player
-        player.update();
-
-        // Update Screen
-        i = (i + 1) % 255;
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
-
-        // Slow Down Rendering (60 FPS)
-        thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
     debug!("Exiting Render Loop...");
 
     Ok(())
+}
+
+fn should_terminate_thread(loopstruct: &RenderLoopStruct) -> bool {
+    let rx: &Receiver<()> = &loopstruct.receive;
+
+    match rx.try_recv() {
+        Ok(_) => {
+            return true;
+        }
+        Err(_) => {
+            return false;
+        }
+    }
+}
+
+extern "C" fn render_loop(loopstruct: &mut RenderLoopStruct) -> bool {
+    if should_terminate_thread(loopstruct) {
+        debug!("Terminating Render Thread...");
+        return true;
+    }
+
+    // Handle Events
+    for event in loopstruct.event_pump.poll_iter() {
+        match event {
+            Event::Quit {..} |
+            Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                debug!("Terminating Render Thread (Quit)...");
+                return true;
+            },
+            Event::ControllerDeviceAdded { which: controller_id, .. } => {
+                let game_controller_subsystem: &GameControllerSubsystem = &loopstruct.game_controller_subsystem;
+                let controller_name: String = game_controller_subsystem.name_for_index(controller_id).unwrap();
+                debug!("Controller {} ({}) Added...", controller_id, controller_name);
+                
+                // TODO: Determine Why Controller Input/Output Error
+                game_controller_subsystem.is_game_controller(controller_id);
+                game_controller_subsystem.open(controller_id).unwrap();
+                // .set_rumble(1000, 1000, 1000).unwrap();
+            },
+            Event::ControllerDeviceRemoved { which: controller_id, .. } => {
+                debug!("Controller {} Removed...", controller_id);
+            },
+            Event::ControllerButtonDown { button: Button::DPadLeft, .. } |
+            Event::KeyDown { keycode: Some(Keycode::Left), repeat: false, .. } => {
+                loopstruct.player.speed = loopstruct.player_movement_speed;
+                loopstruct.player.direction = Direction::Left;
+            },
+            Event::ControllerButtonDown { button: Button::DPadRight, .. } |
+            Event::KeyDown { keycode: Some(Keycode::Right), repeat: false, .. } => {
+                loopstruct.player.speed = loopstruct.player_movement_speed;
+                loopstruct.player.direction = Direction::Right;
+            },
+            Event::ControllerButtonDown { button: Button::DPadUp, .. } |
+            Event::KeyDown { keycode: Some(Keycode::Up), repeat: false, .. } => {
+                loopstruct.player.speed = loopstruct.player_movement_speed;
+                loopstruct.player.direction = Direction::Up;
+            },
+            Event::ControllerButtonDown { button: Button::DPadDown, .. } |
+            Event::KeyDown { keycode: Some(Keycode::Down), repeat: false, .. } => {
+                loopstruct.player.speed = loopstruct.player_movement_speed;
+                loopstruct.player.direction = Direction::Down;
+            },
+            Event::KeyUp { keycode: Some(Keycode::Left), repeat: false, .. } |
+            Event::KeyUp { keycode: Some(Keycode::Right), repeat: false, .. } |
+            Event::KeyUp { keycode: Some(Keycode::Up), repeat: false, .. } |
+            Event::KeyUp { keycode: Some(Keycode::Down), repeat: false, .. } => {
+                loopstruct.player.speed = 0;
+                loopstruct.player.direction = Direction::None;
+            },
+            _ => {}
+        }
+    }
+    
+    // Update Player
+    loopstruct.player.update();
+
+    // Update Screen
+    let mut i: u8 = loopstruct.i.load(Ordering::Relaxed);
+    i = (i + 1) % 255;
+
+    loopstruct.i.store(i, Ordering::Relaxed);
+
+    render(&mut loopstruct.canvas, Color::RGB(i, 64, 255-i), &loopstruct.texture, &loopstruct.player).unwrap();
+
+    // Slow Down Rendering (60 FPS)
+    thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    return false;
 }
 
 // Unused, May Move Render/Event Loop Here
