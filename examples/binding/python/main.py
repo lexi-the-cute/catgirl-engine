eb29293b02325082d33d0c0ee686f54c5c49230e
Alexis <120563837+alexisart@users.noreply.github.com>
2023-06-02 05:16:20 -0400

    Saving Current Work

--- examples/binding/python/main.py
+++ examples/binding/python/main.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python3
 
-import sys
-sys.path.insert(0, 'target/binding')  # This runs from CWD
-sys.path.insert(1, 'target/release')
+# import sys
+# sys.path.insert(0, 'target/binding')  # This runs from CWD
+# sys.path.insert(1, 'target/release')
 
 import pyximport
 pyximport.install()
+# LibLoader
 
 import catgirl_engine
 
 if __name__ == "__main__":
     args: list = []
 
-    # catgirl_engine.SDL_main(0, [])
-
-    print(dir(catgirl_engine))
-    print(catgirl_engine.__loader__) #.SDL_main(len(args), args)
+    catgirl_engine.run(args)
