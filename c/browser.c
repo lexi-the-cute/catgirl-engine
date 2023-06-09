353bf48bfb983a3e0128ce03c6bb6390b67c8a5d
Alexis <alexis@catgirl.land>
2023-06-08 23:17:38 -0400

    Separated Emscripten Support from Browser Support

--- c/browser.c
+++ c/browser.c
@@ -31,15 +31,3 @@ void error(char* message) {
         console.error(UTF8ToString($0, $1));
     }, message, strlen(message));
 }
-
-EMSCRIPTEN_RESULT create_webgl_context() {
-    EmscriptenWebGLContextAttributes attr;
-    // attr.explicitSwapControl = 0;
-    // attr.renderViaOffscreenBackBuffer = 0;
-    attr.proxyContextToMainThread = EMSCRIPTEN_WEBGL_CONTEXT_PROXY_FALLBACK;
-
-    emscripten_webgl_init_context_attributes(&attr);
-    EMSCRIPTEN_WEBGL_CONTEXT_HANDLE ctx = emscripten_webgl_create_context("#canvas", &attr);
-
-    return emscripten_webgl_make_context_current(ctx);
-}
