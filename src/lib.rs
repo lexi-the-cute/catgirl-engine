66e61454573b7d783c277b3898e7630cf95c80ac
Alexis <120563837+alexisart@users.noreply.github.com>
2023-06-21 23:02:17 -0400

    Libs

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,23 @@
 
 #[macro_use] extern crate log;
 
+use core::ffi::{c_char, c_int};
+
 mod game;
 mod server;
 mod client;
 
 // Run as Library (e.g. Android and WebAssembly)
 #[start]
+#[no_mangle]
 #[allow(non_snake_case)]
-pub extern fn main(_argc: isize, _argv: *const *const u8) -> isize {
+pub extern fn ce_start(argc: c_int, argv: *const *const c_char) -> c_int {
     game::setup_logger();
     debug!("Launched as library...");
     
-    return game::start(_argc, _argv);
+    // This is to convert from C Main Args to Rust Main Args
+    let _argc: isize = argc.try_into().unwrap();
+    let _argv: *const *const u8 = argv as *const *const u8;
+
+    return game::start(_argc, _argv).try_into().unwrap();
 }
