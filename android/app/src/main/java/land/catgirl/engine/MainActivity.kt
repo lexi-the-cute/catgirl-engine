c99b745b100479d7af65fadebef6f676600d6dd2
Alexis <alexis@catgirl.land>
2023-06-23 03:46:59 -0400

    Saving Current Work

--- android/app/src/main/java/land/catgirl/engine/MainActivity.kt
+++ android/app/src/main/java/land/catgirl/engine/MainActivity.kt
@@ -1,7 +1,11 @@
 package land.catgirl.engine
 
+import android.os.Bundle
 import android.view.View
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.google.androidgamesdk.GameActivity
+
 
 class MainActivity : GameActivity() {
     companion object {
@@ -12,18 +16,26 @@ class MainActivity : GameActivity() {
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
+
         if (hasFocus) {
             hideSystemUi()
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     private fun hideSystemUi() {
         val decorView = window.decorView
-        decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
+        val controller = WindowInsetsControllerCompat(
+            window,
+            decorView
+        )
+
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.hide(WindowInsetsCompat.Type.displayCutout())
+        controller.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
