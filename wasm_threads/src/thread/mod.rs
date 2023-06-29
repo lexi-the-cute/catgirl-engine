26605e20e8770873390b39635563da5b957c33c8
Alexis <alexis@catgirl.land>
2023-06-29 04:45:36 -0400

    ???

--- wasm_threads/src/thread/mod.rs
+++ wasm_threads/src/thread/mod.rs
@@ -1,7 +1,11 @@
 // use wasm_bindgen::{prelude::wasm_bindgen, JsError};
 // use std::io::Result;
-use core::fmt::Error;
+// use core::fmt::Error;
 use core::result::Result;
+
+use web_sys;
+use js_sys;
+use wasm_bindgen::JsValue;
 
 #[derive(Debug)]
 pub struct Builder {
@@ -17,11 +21,32 @@ impl Builder {
         }
     }
 
-    #[allow(unused_variables, unreachable_code)]
-    pub fn spawn<F, T>(self, f: F) -> Result<JoinHandle<T>, Error> {
-        // Result
+    // #[allow(unused_variables, unreachable_code)]
+    // pub fn spawn<F, T>(self, f: F) -> Result<JoinHandle<T>, Error> {
+    //     // Result
 
-        Ok(JoinHandle(todo!()))
+    //     Ok(JoinHandle(todo!()))
+    // }
+
+    pub fn spawn<F, T>(self, f: F) -> Result<JoinHandle<T>, JsValue>
+    where F: FnOnce() -> T, F: Send + 'static, T: Send + 'static {
+        let worker: web_sys::Worker = web_sys::Worker::new("./worker.js")?;
+        let ptr: *mut F = Box::into_raw(Box::new(f));
+        let msg: js_sys::Array = js_sys::Array::new();
+
+        msg.push(&wasm_bindgen::memory());
+        msg.push(&JsValue::from(ptr as u32));
+        let _: Result<(), JsValue> = worker.post_message(&msg);
+
+        error!("Testing 1");
+        todo!();
+        let result: T = f();
+        error!("Testing 2");
+
+        Ok(JoinHandle {
+            result,
+            worker
+        })
     }
 
     pub fn name(mut self, name: String) -> Builder {
@@ -37,7 +62,10 @@ impl Builder {
     }
 }
 
-pub struct JoinHandle<T>(T);
+pub struct JoinHandle<T> {
+    result: T,
+    worker: web_sys::Worker
+}
 
 impl<T> JoinHandle<T> {
     pub fn is_finished(&self) -> bool {
