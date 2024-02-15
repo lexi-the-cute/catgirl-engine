54e9ce5bcb409b3685e78e42082d6b91361efa57
Alexis <alexis@catgirl.land>
2024-02-15 04:23:41 -0500

    Full Screen

--- android/app/src/main/java/land/catgirl/engine/MainActivity.kt
+++ android/app/src/main/java/land/catgirl/engine/MainActivity.kt
@@ -2,6 +2,7 @@ package land.catgirl.engine
 
 import android.os.Bundle
 import android.util.Log
+import android.view.WindowManager
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.google.androidgamesdk.GameActivity
@@ -15,30 +16,38 @@ class MainActivity : GameActivity() {
         super.onCreate(savedInstanceState)
 
         Log.d(TAG, "Started Main Activity...")
+
+        hideSystemUi()
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
 
-        if (hasFocus) {
-            hideSystemUi()
-        }
+//        if (hasFocus) {
+//            hideSystemUi()
+//        }
     }
 
     private fun hideSystemUi() {
         Log.d(TAG, "Hiding System UI...")
 
-        val decorView = window.decorView
-        val controller = WindowInsetsControllerCompat(
-            window,
-            decorView
-        )
+        // Log.v(TAG, "Decor View: ${window.decorView}; Tag: ${window.decorView.tag}")
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
 
-        // Log.v(TAG, "Decor View: $decorView; Tag: ${decorView.tag}")
+        // STATUS_BARS - Notification Icons Bar
+        // NAVIGATION_BARS - The 3 buttons at the bottom
+        // CAPTION_BAR - ???
         controller.hide(WindowInsetsCompat.Type.systemBars())
-        controller.hide(WindowInsetsCompat.Type.displayCutout())
-        controller.systemBarsBehavior =
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+
+        // DISPLAY_CUTOUT - The area which extends past the normal screen
+        // controller.hide(WindowInsetsCompat.Type.displayCutout())
+        window.attributes.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES
+
+        // The bar that says Catgirl Engine at the top
+        supportActionBar?.hide()
+
+        // Allow users to show bars by swiping
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 
    companion object {
