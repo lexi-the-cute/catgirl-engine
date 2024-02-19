594df08328b939a6d3f4da6f323a8ca4e9f5d69f
Alexis <alexis@catgirl.land>
2024-02-18 23:40:06 -0500

    saving comments

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,11 @@
 // TODO: Figure out what is causing gradle to find an empty path and fix
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // https://mvnrepository.com
     id("com.android.application") version "8.2.2" apply false
     id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    // https://plugins.gradle.org/plugin/com.github.willir.rust.cargo-ndk-android
     id("com.github.willir.rust.cargo-ndk-android") version "0.3.4" apply false
 }
