5d3c9edc8de326f805f24d945e97e323730466e0
Alexis <alexis@catgirl.land>
2024-02-18 23:12:35 -0500

    added comments

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,4 @@
+// TODO: Figure out what is causing gradle to find an empty path and fix
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.2.2" apply false
