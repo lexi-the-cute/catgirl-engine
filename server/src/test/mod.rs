03bb5cec9c8cbfce813dc9d60bb6111c78e88fc1
Alexis <alexis@catgirl.land>
2024-02-28 04:00:06 -0500

    started work on adding tests

---
+++ server/src/test/mod.rs
@@ -0,0 +1,1 @@
+//! Tests for the server side of the catgirl-engine crate
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@
 #![warn(missing_docs)]
 #![warn(clippy::missing_docs_in_private_items)]
 
+#[cfg(test)]
+/// Handles testing the code
+mod test;
+
 #[macro_use]
 extern crate tracing;
 
