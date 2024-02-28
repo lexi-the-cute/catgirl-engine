54d0c6d33dcff268ec79845aa25975ca01726118
Alexis <alexis@catgirl.land>
2024-02-28 04:35:50 -0500

    adding publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo Stashing all uncommited data
 git add --all
 git stash
@@ -6,19 +8,19 @@ export VERSION=`cat ./Cargo.toml | grep '^version' | head -n1 | cut -d'"' -f2 | 
 echo Publish catgirl-enging v$VERSION
 
 echo Replacing version number in catgirl-engine-client with catgirl-engine version
-sed -i 's/^version = "[0-9.]*"/version = "`$VERSION`"/' ./client/Cargo.toml
+sed -i "s/^version = \"[0-9.]*\"/version = \"$VERSION\"/" ./client/Cargo.toml
 
 echo Replacing version number in catgirl-engine-server with catgirl-engine version
-sed -i 's/^version = "[0-9.]*"/version = "`$VERSION`"/' ./server/Cargo.toml
+sed -i "s/^version = \"[0-9.]*\"/version = \"$VERSION\"/" ./server/Cargo.toml
 
-# echo Publishing catgirl-engine-client
-# cargo publish -p catgirl-engine-client --allow-dirty
+echo Publishing catgirl-engine-client
+cargo publish -p catgirl-engine-client --allow-dirty
 
-# echo Publishing catgirl-engine-server
-# cargo publish -p catgirl-engine-server --allow-dirty
+echo Publishing catgirl-engine-server
+cargo publish -p catgirl-engine-server --allow-dirty
 
-# echo Publishing catgirl-engine
-# cargo publish -p catgirl-engine --allow-dirty
+echo Publishing catgirl-engine
+cargo publish -p catgirl-engine --allow-dirty
 
 echo Resetting client version
 git checkout HEAD -- ./client/Cargo.toml
