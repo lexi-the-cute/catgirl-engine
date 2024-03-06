e35e4319a7ceb119389ed1d9df7ee8e7c1680b0f
Alexis <alexis@catgirl.land>
2024-03-06 00:57:49 -0500

    python execution

--- examples/binding/python/catgirl_engine.pyx
+++ examples/binding/python/catgirl_engine.pyx
@@ -1,15 +1,20 @@
-# cython: language_level=3
+# cython: language_level=3, c_string_encoding=utf8
 # distutils: libraries = main
 # distutils: library_dirs = ../../../target/release
 # distutils: include_dirs = ../../../target/binding
 
 from libc.stdlib cimport malloc, free
 
+import os
+
 # Import catgirl_engine.pxd
 cimport catgirl_engine
 
 
 def run(args: list):
+    # Ensures the program is listed as the first arg
+    args.insert(0, os.path.realpath(__file__))
+
     cdef const char** c_args
     c_args = <const char**> malloc(sizeof(const char*)*len(args))
 
