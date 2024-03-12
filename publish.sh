579553fb3969a763a1f35e59bbb3e48fea6d0b65
Alexis <alexis@catgirl.land>
2024-03-12 02:01:59 -0400

    test local

--- publish.sh
+++ publish.sh
@@ -4,24 +4,7 @@ echo Stashing all uncommitted data
 git add --all
 git stash
 
-export VERSION=`cat ./Cargo.toml | grep '^version' | head -n1 | cut -d'"' -f2 | tr -d '\n'`
-echo Publish catgirl-engine v$VERSION
-
-echo Replacing version number in catgirl-engine-utils with catgirl-engine version
-sed -i "s/^version = \"[0-9.]*\"/version = \"$VERSION\"/" ./utils/Cargo.toml
-
-echo Replacing version number in catgirl-engine-client with catgirl-engine version
-sed -i "s/^version = \"[0-9.]*\"/version = \"$VERSION\"/" ./client/Cargo.toml
-sed -i "s/^utils = { version = \"[0-9.]*\"/utils = { version = \"$VERSION\"/" ./client/Cargo.toml
-
-echo Replacing version number in catgirl-engine-server with catgirl-engine version
-sed -i "s/^version = \"[0-9.]*\"/version = \"$VERSION\"/" ./server/Cargo.toml
-sed -i "s/^utils = { version = \"[0-9.]*\"/utils = { version = \"$VERSION\"/" ./server/Cargo.toml
-
-echo Replacing version number in catgirl-engine dependencies with catgirl-engine version
-sed -i "s/^utils = { version = \"[0-9.]*\"/utils = { version = \"$VERSION\"/" ./Cargo.toml
-sed -i "s/^client = { version = \"[0-9.]*\"/client = { version = \"$VERSION\"/" ./Cargo.toml
-sed -i "s/^server = { version = \"[0-9.]*\"/server = { version = \"$VERSION\"/" ./Cargo.toml
+./version.sh
 
 echo Publishing catgirl-engine-utils
 cargo publish -p catgirl-engine-utils --allow-dirty --no-verify
