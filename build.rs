a3fd48bc1f2743c983ccfead329adbb4263cf849
Alexis <alexis@catgirl.land>
2024-03-14 01:50:27 -0400

    testing doc building --publish

--- build.rs
+++ build.rs
@@ -37,7 +37,10 @@ fn generate_build_info() {
         depth = DependencyDepth::None;
     }
 
-    build_info_build::build_script().collect_runtime_dependencies(depth);
+    let options: build_info_build::BuildScriptOptions = build_info_build::build_script();
+    if !docs_rs {
+        options.collect_runtime_dependencies(depth);
+    }
 }
 
 fn set_rustflags() {
