b8b0f302e080e4910f5c9126f2fe107927d6ad2e
Alexis <alexis@catgirl.land>
2024-03-17 01:59:21 -0400

    agghhhhhhhhhhhhhh

--- server/build.rs
+++ server/build.rs
@@ -1,10 +1,8 @@
-#[cfg(feature = "build-info")]
 use build_info_build::DependencyDepth;
 use std::env;
 
 fn main() {
     // Generate build info
-    #[cfg(feature = "build-info")]
     generate_build_info();
 }
 
@@ -14,7 +12,6 @@ fn matches_environment_var(key: &str, value: &str) -> bool {
     environment_var.is_ok() && environment_var.unwrap() == value
 }
 
-#[cfg(feature = "build-info")]
 fn generate_build_info() {
     let mut depth: DependencyDepth = DependencyDepth::Depth(0);
 
