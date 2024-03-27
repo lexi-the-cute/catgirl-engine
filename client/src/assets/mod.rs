ce1e1bb0c7979821e7525e00745241214b6c950e
Alexis <alexis@catgirl.land>
2024-03-27 05:39:01 -0400

    experiment macros clippy

--- client/src/assets/mod.rs
+++ client/src/assets/mod.rs
@@ -4,7 +4,7 @@
 #[macro_export]
 macro_rules! load_bytes {
     ($file:literal) => {{
-        let external_assets_path: PathBuf = crate::game::get_assets_path().join($file);
+        let external_assets_path: PathBuf = $crate::game::get_assets_path().join($file);
         trace!("External Assets Path (Bytes): {:?}", external_assets_path);
 
         let embedded_bytes: Vec<u8> = std::fs::read(&external_assets_path).unwrap_or_else(|_| {
@@ -23,7 +23,7 @@ macro_rules! load_bytes {
 #[macro_export]
 macro_rules! load_string {
     ($file:literal) => {{
-        let external_assets_path: PathBuf = crate::game::get_assets_path().join($file);
+        let external_assets_path: PathBuf = $crate::game::get_assets_path().join($file);
         trace!("External Assets Path (String): {:?}", external_assets_path);
 
         let embedded_string: String = std::fs::read_to_string(&external_assets_path)
