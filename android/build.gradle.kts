580d6dc952f64c47d6a80e5b3787532a46e60ad2
Alexis <alexis@catgirl.land>
2024-04-08 00:46:46 -0400

    testing --publish

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     // https://mvnrepository.com
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.application") version "8.3.1" apply false
+    id("com.android.library") version "8.3.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 
     // https://plugins.gradle.org/plugin/com.github.willir.rust.cargo-ndk-android
