f0f79c72b8667df32638bf157fac6f1b3356a768
Alexis <alexis@catgirl.land>
2024-11-08 09:39:43 -0800

    testing build fix 2 --publish

--- examples/wasm/web/build.sh
+++ examples/wasm/web/build.sh
@@ -19,27 +19,27 @@ cp -a $PROJECT_ROOT/client/assets $SCRIPT_DIR/assets
 
 echo "Compiling Game Engine..."
 if [ $PROFILE == "debug" ]; then
-    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown
+    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown --bin catgirl-engine
 else
-    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown --release
+    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown --release --bin catgirl-engine
 fi
 
 echo "Generating Usable Wasm Binary and Supporting Files..."
 if [ $PROFILE == "debug" ]; then
-    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/debug/main.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web --debug --keep-debug
+    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/debug/catgirl-engine.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web --debug --keep-debug
 else
-    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/release/main.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web
+    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/release/catgirl-engine.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web
 fi
 
 if [ $PROFILE != "debug" ]; then
     echo "Optimizing Wasm Binary For Size..."
-    wasm-opt $SCRIPT_DIR/pkg/main_bg.wasm -o $SCRIPT_DIR/pkg/main_bg.opt.wasm -O
-    mv $SCRIPT_DIR/pkg/main_bg.opt.wasm $SCRIPT_DIR/pkg/main_bg.wasm
+    wasm-opt $SCRIPT_DIR/pkg/catgirl-engine_bg.wasm -o $SCRIPT_DIR/pkg/catgirl-engine_bg.opt.wasm -O
+    mv $SCRIPT_DIR/pkg/catgirl-engine_bg.opt.wasm $SCRIPT_DIR/pkg/catgirl-engine_bg.wasm
 fi
 
 if [ $PROFILE == "debug" ] && [ $ENABLE_SOURCES == "true" ]; then
     echo "Create Wasm Source Map..."
-    cargo wasm2map $SCRIPT_DIR/pkg/main_bg.wasm --patch --base-url $HOST
+    cargo wasm2map $SCRIPT_DIR/pkg/catgirl-engine_bg.wasm --patch --base-url $HOST
 fi
 
 if [ $PROFILE == "debug" ] && [ $ENABLE_SOURCES == "true" ]; then
