a8b4462ad94bd88af6be3565cb3f307e2b834291
Alexis <alexis@catgirl.land>
2024-11-08 02:27:19 -0800

    testing new wasm generation rules --publish

--- examples/wasm/web/build.sh
+++ examples/wasm/web/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+PROFILE="debug"  # "debug" or "release"
 HOST=http://127.0.0.1:8000/pkg
 # RUST_LOG=info
 
@@ -10,18 +11,27 @@ PROJECT_ROOT=`cd $SCRIPT_DIR/../../.. && pwd`
 sed "s/%CACHE_VERSION%/`date +'%s'`/" $SCRIPT_DIR/service-worker.js.template > $SCRIPT_DIR/service-worker.js
 
 rm -r $SCRIPT_DIR/assets
-cp -a $PROJECT_ROOT/License.md $PROJECT_ROOT/LICENSE
 cp -a $PROJECT_ROOT/client/assets $SCRIPT_DIR/assets
 
-# Generates usable Wasm binary and supporting files
-# wasm-pack build --profiling --target web -d $SCRIPT_DIR/pkg
-wasm-pack build --dev --target web -d $SCRIPT_DIR/pkg
-# wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/debug/main.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web --debug
+echo "Compiling Game Engine..."
+if [ $PROFILE == "debug" ]; then
+    cargo build --target wasm32-unknown-unknown
+else
+    cargo build --target wasm32-unknown-unknown --release
+fi
 
-# Creates Wasm Source Map to aid in debugging
+echo "Generating Usable Wasm Binary and Supporting Files..."
+if [ $PROFILE == "debug" ]; then
+    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/debug/main.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web --debug --keep-debug
+else
+    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/release/main.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web
+fi
+
+if [ $PROFILE != "debug" ]; then
+    echo "Optimizing Wasm Binary For Size..."
+    wasm-opt $SCRIPT_DIR/pkg/main_bg.wasm -o $SCRIPT_DIR/pkg/main_bg.opt.wasm -O
+    mv $SCRIPT_DIR/pkg/main_bg.opt.wasm $SCRIPT_DIR/pkg/main_bg.wasm
+fi
+
+echo "Create Wasm Source Map..."
 cargo wasm2map $SCRIPT_DIR/pkg/main_bg.wasm --patch --base-url $HOST
-
-# cargo build --target wasm32-unknown-unknown
-# wasm-bindgen --keep-debug --web --out-dir pkg ./target/wasm32-unknown-unknown/
-
-rm $PROJECT_ROOT/LICENSE
