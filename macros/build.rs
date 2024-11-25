4099ed0a890863e1853c22c5676dfb93529820d5
Alexis <alexis@catgirl.land>
2024-11-25 10:50:22 -0800

    testing cargo packaging --publish

--- macros/build.rs
+++ macros/build.rs
@@ -1,4 +1,17 @@
 //! Build script for crate
 
 /// Main function
-fn main() {}
+fn main() {
+    // Debug environment
+    print_environment_vars();
+}
+
+/// Print all environment variables
+#[allow(dead_code)]
+fn print_environment_vars() {
+    let vars: std::env::Vars = std::env::vars();
+
+    for (key, var) in vars {
+        println!("cargo:warning=EV: {key}: {var}");
+    }
+}
