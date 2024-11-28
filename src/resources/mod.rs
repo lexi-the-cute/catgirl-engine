ec7b65f39805cb03f2a4be72ce55e78ae59f7353
Alexis <alexis@catgirl.land>
2024-11-28 00:47:18 -0800

    re-export

--- src/resources/mod.rs
+++ src/resources/mod.rs
@@ -1,4 +1,3 @@
 // Where resources are embedded in the binary
 // #[cfg(feature = "embed-resources")]
-// env!("ENGINE_RESOURCES_PATH")
 macros::generate_embedded_resources!(env!("ENGINE_RESOURCES_PATH"));
