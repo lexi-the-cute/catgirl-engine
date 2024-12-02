a96556f6aaf14afb296811342b49afe78edbd765
Alexis <alexis@catgirl.land>
2024-12-02 14:21:05 -0800

    working on platform implementation separation

--- examples/wasm/web/build.sh
+++ examples/wasm/web/build.sh
@@ -14,32 +14,34 @@ LIBRARY_PATH="$HOME/.rustup/toolchains/$TOOLCHAIN-x86_64-unknown-linux-gnu/lib/r
 sed "s/%CACHE_VERSION%/`date +'%s'`/" $SCRIPT_DIR/service-worker.js.template > $SCRIPT_DIR/service-worker.js
 
 rm -r $SCRIPT_DIR/pkg
-rm -r $SCRIPT_DIR/assets
-cp -a $PROJECT_ROOT/client/assets $SCRIPT_DIR/assets
+rm -r $SCRIPT_DIR/resources
+
+mkdir -p $SCRIPT_DIR/resources
+cp -a $PROJECT_ROOT/resources/assets $SCRIPT_DIR/resources/assets
 
 echo "Compiling Game Engine..."
 if [ $PROFILE == "debug" ]; then
-    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown --bin catgirl-engine
+    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown --lib
 else
-    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown --release --bin catgirl-engine
+    cargo +$TOOLCHAIN build --target wasm32-unknown-unknown --release --lib
 fi
 
 echo "Generating Usable Wasm Binary and Supporting Files..."
 if [ $PROFILE == "debug" ]; then
-    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/debug/catgirl-engine.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web --debug --keep-debug
+    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/debug/main.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web --debug --keep-debug
 else
-    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/release/catgirl-engine.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web
+    wasm-bindgen $PROJECT_ROOT/target/wasm32-unknown-unknown/release/main.wasm --out-dir $SCRIPT_DIR/pkg --typescript --target web
 fi
 
 if [ $PROFILE != "debug" ]; then
     echo "Optimizing Wasm Binary For Size..."
-    wasm-opt $SCRIPT_DIR/pkg/catgirl-engine_bg.wasm -o $SCRIPT_DIR/pkg/catgirl-engine_bg.opt.wasm -Oz
-    mv $SCRIPT_DIR/pkg/catgirl-engine_bg.opt.wasm $SCRIPT_DIR/pkg/catgirl-engine_bg.wasm
+    wasm-opt $SCRIPT_DIR/pkg/main_bg.wasm -o $SCRIPT_DIR/pkg/main_bg.opt.wasm -Oz
+    mv $SCRIPT_DIR/pkg/main_bg.opt.wasm $SCRIPT_DIR/pkg/main_bg.wasm
 fi
 
 if [ $PROFILE == "debug" ] && [ $ENABLE_SOURCES == "true" ]; then
     echo "Create Wasm Source Map..."
-    cargo wasm2map $SCRIPT_DIR/pkg/catgirl-engine_bg.wasm --patch --base-url $HOST
+    cargo wasm2map $SCRIPT_DIR/pkg/main_bg.wasm --patch --base-url $HOST
 fi
 
 # if [ $PROFILE == "debug" ] && [ $ENABLE_SOURCES == "true" ]; then
