1b763b52d571b84def4260d570df9f6f5a1dc57d
Alexis <alexis@catgirl.land>
2024-12-02 19:46:54 -0800

    docs stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
 //! Starting point for the game engine as a library
-
+#![doc(
+    html_favicon_url = "https://engine.catgirl.land/resources/assets/vanilla/texture/logo/logo.svg",
+    html_logo_url = "https://engine.catgirl.land/resources/assets/vanilla/texture/logo/logo.svg",
+    html_playground_url = "https://play.rust-lang.org"
+)]
 #![warn(missing_docs)]
 
 #[macro_use]
