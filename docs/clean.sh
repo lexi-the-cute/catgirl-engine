2d1476e91f43fbe2396244924609378dab741443
Alexis <alexis@catgirl.land>
2024-12-10 19:58:26 -0800

    clean

--- docs/clean.sh
+++ docs/clean.sh
@@ -5,6 +5,10 @@ SCRIPT=`realpath "$0"`
 SCRIPT_DIR=`dirname "$SCRIPT"`
 PROJECT_ROOT=`cd $SCRIPT_DIR/.. && pwd`
 
+rm $SCRIPT_DIR/manifest.json
 rm $SCRIPT_DIR/service-worker.js
 rm -r $SCRIPT_DIR/pkg
+rm -r $SCRIPT_DIR/_site
 rm -r $SCRIPT_DIR/resources
+rm -r $SCRIPT_DIR/.sass-cache
+rm -r $SCRIPT_DIR/.jekyll-cache
